1cd45370ffc143d8ea14ea421a61e138794b9a54
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2016-01-03 19:10:02 +0100

    Fine tune numeric parsing logic

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,13 @@
 # Change Log
 
 
+## 0.1.1 - 2015-01-03
+
+### Changed
+
+- Support parsing "unusual" numeric repesentations
+
+
 ## 0.1.0 - 2015-01-03
 
 ### Added
--- src/Parser.php
+++ src/Parser.php
@@ -93,13 +93,15 @@ class Parser
         }
 
         // Normalize numeric value
-        if (is_numeric($value) && ((string) ($value + 0) === $value)) {
-            $value = $value + 0;
+        if (is_numeric($value)) {
+            $numericValue = $value + 0;
 
             // Typed ini parsing does not support negative doubles
             // https://3v4l.org/ujDo1
-            if (is_float($value) and $value < 0) {
-                $value = (string) $value;
+            if ((is_int($numericValue) && (int)$value === $numericValue)
+                || (is_float($numericValue) && (float)$value === $numericValue && $numericValue > 0)
+            ) {
+                $value = $numericValue;
             }
         }
 
